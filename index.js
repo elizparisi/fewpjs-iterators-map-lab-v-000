9f4fce8730a04d6accf40292c2c545a744ccfa0f
Liz P <parisielizabeth@gmail.com>
2020-08-16 05:44:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,23 +17,11 @@ const tutorials = [
 */
 
 const titleCased = () => {
-  return tutorials.map(tutorial => {tutorial = tutorial.split(' ');
+  return tutorials.map(tutorial => {tutorial = tutorial.split(' ')
   
   for (let i = 0; i < tutorial.length; i++){
-    tutorial[i] = `${tutorial[i].charAt(0).toUpperCase()}${tutorial[i].slice(1)}`;
+    tutorial[i] = `${tutorial[i].charAt(0).toUpperCase()}${tutorial[i].slice(1)}`
     }
     return tutorial.join(' ');
   });
 };
-
-
-const titleCased = () => {
-  return tutorials.map(tutorial => {
-    tutorial = tutorial.split(' ')
-
-    for (let i = 0; i < tutorial.length; i++) {
-      tutorial[i] = `${tutorial[i].charAt(0).toUpperCase()}${tutorial[i].slice(1)}`
-    }
-    return tutorial.join(' ')
-  })
-}
