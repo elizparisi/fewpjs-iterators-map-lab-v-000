8ae2de1561def19e308091126cf360ba0052ab81
Liz P <parisielizabeth@gmail.com>
2020-08-16 05:38:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ const titleCased = (){
   for (let i=0; i < tutorial.length; i++){
     tutorial[i] = `${tutorial[i].charAt(0).toUpperCase()}${tutorial[i].slice(1)}`
     }
-  return tutorial.join(' ');
+    return tutorial.join(' ');
   });
 }
