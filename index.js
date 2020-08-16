6bc004b89507fd574e73cc8a60e5d81ac53ff8d6
Liz P <parisielizabeth@gmail.com>
2020-08-16 05:37:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const tutorials = [
 */
 
 const titleCased = (){
-  tutorials.map(tutorial => tutorial.split(' ')
+  return tutorials.map(tutorial => tutorial.split(' ')
   
   for (let i=0; i < tutorial.length; i++){
     tutorial[i] = `${tutorial[i].charAt(0).toUpperCase()}${tutorial[i].slice(1)}`
